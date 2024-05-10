159d2f8a59d55bdb2e659edd5109b56c3f4e9f77
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-05-10 17:00:32 +0500

    sectionList is completed

--- my-app/components/MenuItem.tsx
+++ my-app/components/MenuItem.tsx
@@ -1,72 +1,85 @@
-import { StyleSheet, Text, View,FlatList } from 'react-native'
+import { StyleSheet, Text, View,FlatList, SectionList } from 'react-native'
 import React from 'react'
 
 interface MenuItem{
-  name:string;
-  price:string,
-  id:string
+  title:string;
+  data:string[];
 }
 
 
-const Item=({name,price}:{name:string,price:string})=>{
+const Item=({name}:{name:string})=>{
   return (
     <View style={{margin:15,marginHorizontal:30}}>
       {/* <Text style={{fontSize:25,color:"#F4CE14"}}>{name}</Text> */}
-      <View style={{flexDirection: "row"}}>
+
       <Text style={{fontSize:25,color:"#F4CE14",width:240}}>{name}</Text>
-      <Text style={{fontSize:25,color:"#F4CE14"}}>{price}</Text>
-      </View>
       
       
     </View>
   )
 }
-
-const SepartorComponent=()=> <View style={{borderBottomColor:"white",borderBottomWidth:1}}/>
+const SeparatorComponent=()=> <View style={{borderBottomColor:"white",borderBottomWidth:1}}/>
 const ItemHeader=()=> <View><Text style={{color:"white"}}>Items</Text></View>
 const ItemFooter=()=> <View><Text style={{color:"white"}}>Items Finish</Text></View>
+
+
 
 const MenuItem = () => {
 
   const menuItemsToDisplay:MenuItem[] = [
-    { name: 'Hummus', price: '$5.00', id: '1A' },
-    { name: 'Moutabal', price: '$5.00', id: '2B' },
-    { name: 'Falafel', price: '$7.50', id: '3C' },
-    { name: 'Marinated Olives', price: '$5.00', id: '4D' },
-    { name: 'Kofta', price: '$5.00', id: '5E' },
-    { name: 'Eggplant Salad', price: '$8.50', id: '6F' },
-    { name: 'Lentil Burger', price: '$10.00', id: '7G' },
-    { name: 'Smoked Salmon', price: '$14.00', id: '8H' },
-    { name: 'Kofta Burger', price: '$11.00', id: '9I' },
-    { name: 'Turkish Kebab', price: '$15.50', id: '10J' },
-    { name: 'Fries', price: '$3.00', id: '11K' },
-    { name: 'Buttered Rice', price: '$3.00', id: '12L' },
-    { name: 'Bread Sticks', price: '$3.00', id: '13M' },
-    { name: 'Pita Pocket', price: '$3.00', id: '14N' },
-    { name: 'Lentil Soup', price: '$3.75', id: '15O' },
-    { name: 'Greek Salad', price: '$6.00', id: '16Q' },
-    { name: 'Rice Pilaf', price: '$4.00', id: '17R' },
-    { name: 'Baklava', price: '$3.00', id: '18S' },
-    { name: 'Tartufo', price: '$3.00', id: '19T' },
-    { name: 'Tiramisu', price: '$5.00', id: '20U' },
-    { name: 'Panna Cotta', price: '$5.00', id: '21V' },
+    {
+      title: 'Appetizers',
+      data: [
+        'shoaib',
+        'Moutabal',
+        'Falafel',
+        'Marinated Olives',
+        'Kofta',
+        'Eggplant Salad',
+      ],
+    },
+    {
+      title: 'Main Dishes',
+      data: ['Lentil Burger', 'Smoked Salmon', 'Kofta Burger', 'Turkish Kebab'],
+    },
+    {
+      title: 'Sides',
+      data: [
+        'Fries',
+        'Buttered Rice',
+        'Bread Sticks',
+        'Pita Pocket',
+        'Lentil Soup',
+        'Greek Salad',
+        'Rice Pilaf',
+      ],
+    },
+    {
+      title: 'Desserts',
+      data: ['Baklava', 'Tartufo', 'Tiramisu', 'Panna Cotta'],
+    },
   ];
+  
 
-  const renderItem=({item}:{item:MenuItem})=>{
+  const renderItem=({item}:{item:string})=>{
     return(
-    <Item name={item.name} price={item.price}/>
+    <Item name={item}/>
     )
   }
+const renderSectionHeader=({section:{title}}:{section:MenuItem})=> <Text>{title}</Text>
+
 
   return (
     <View>
-      <FlatList 
-      data={menuItemsToDisplay}
-      renderItem={renderItem} 
-      keyExtractor={(item)=>item.id}
-      ItemSeparatorComponent={SepartorComponent}
-      ListHeaderComponent={ItemHeader}
-      ListFooterComponent={ItemFooter}></FlatList>
+      <SectionList
+      keyExtractor={(item,index)=>"item"+index}
+      sections={menuItemsToDisplay}
+      renderItem={renderItem}
+      renderSectionHeader={renderSectionHeader}
+      SectionSeparatorComponent={SeparatorComponent}
+        ListHeaderComponent={ItemHeader}
+        ListFooterComponent={ItemFooter}
+      ></SectionList>
     </View>
   )
 }
