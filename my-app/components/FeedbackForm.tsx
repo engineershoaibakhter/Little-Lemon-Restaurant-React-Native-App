b333032ceb02cb8f088d3013b8c785d6db10e57a
Shoaib Akhter <shoaibakhter181422@gmail.com>
2024-05-10 19:06:11 +0500

    learnt keyboardAvoidingView with its bahaviour

--- my-app/components/FeedbackForm.tsx
+++ my-app/components/FeedbackForm.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, TextInput, ScrollView } from "react-native";
+import { StyleSheet, Text, View, TextInput, ScrollView,KeyboardAvoidingView, Platform } from "react-native";
 import React from "react";
 
 const FeedbackForm = () => {
@@ -8,9 +8,16 @@ const FeedbackForm = () => {
 
     
   return (
-    <ScrollView>
+    <ScrollView >
+        <KeyboardAvoidingView behavior={Platform.OS==="ios"?"padding":"height"}>
       <Text style={{textAlign:"center",fontSize:20}}>How was your visit to Little Lemon? </Text>
       <Text>
+        Little Lemon is a charming neighborhood bistro that serves simple food
+        and classic cocktails in a lively but casual environment. We would love
+        to hear your experience with us!{" "} 
+        Little Lemon is a charming neighborhood bistro that serves simple food
+        and classic cocktails in a lively but casual environment. We would love
+        to hear your experience with us!{" "} 
         Little Lemon is a charming neighborhood bistro that serves simple food
         and classic cocktails in a lively but casual environment. We would love
         to hear your experience with us!{" "}
@@ -18,7 +25,7 @@ const FeedbackForm = () => {
       <TextInput style={{backgroundColor:"black",marginHorizontal:20,borderRadius:20,color:"white",padding:8,margin:10}} value={firstName} onChangeText={setFirstName} placeholder="First Name" placeholderTextColor={"white"}></TextInput>
       <TextInput style={{backgroundColor:"black",marginHorizontal:20,borderRadius:20,color:"white",padding:8,margin:10}} value={lastName} onChangeText={setLastName} placeholder="Last Name" placeholderTextColor={"white"}></TextInput>
       <TextInput style={{backgroundColor:"black",marginHorizontal:20,borderRadius:20,color:"white",padding:8,margin:10}} value={message} onChangeText={setMessage} placeholder="Message" placeholderTextColor={"white"}></TextInput>
-
+      </KeyboardAvoidingView>
     </ScrollView>
   );
 };
